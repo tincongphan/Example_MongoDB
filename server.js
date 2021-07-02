17bf89b248454e6d8b923edd2ab33983b6109b22
tin cong phan <tincongphan@gmail.com>
2021-07-02 14:58:53 +0700

    connect Nodejs to MongoDB

---
+++ server.js
@@ -0,0 +1,6 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/courses", {useNewUrlParser: true, useUnifiedTopology: true})
+    .then( () => console.log("Connected to MongoDB"))
+    .catch( (error) => console.log(error) )
